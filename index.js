160152be70db1aa90e8cb45d5b2fba10c232d111
Fathi <fatximohamed999@gmail.com>
2026-03-24 10:37:57 +0300

    Add new file

--- index.js
+++ index.js
@@ -2,10 +2,5 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello DevOps World!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.get('/', (req, res) => res.send('Hello DevOps World!'));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
